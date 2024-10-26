0d7413579711cf685f3dc98d25935945d2978389
Brendan Degryse <brendan.degryse@westpoint.edu>
2024-10-26 14:41:21 -0400

    Update

--- milestone_b_starter_files/tcp_server.py
+++ milestone_b_starter_files/tcp_server.py
@@ -108,7 +108,8 @@ class Server:
                 pkt = Datagram.from_bytes(self.server_socket.recv(self.frame_size))
             except socket.timeout as e:
                 print(e)
-                continue
+                print("Bad bad bad")
+                # continue
             if pkt.dest_port != self.server_port:
                 continue
             if pkt.seq_num == self.ack_num:
